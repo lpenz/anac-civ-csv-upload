f04a1b27448d2c0479c24e9f1410fd2e757b6f7e
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-01-31 21:38:03 +0000

    Add license and more to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
+#!/usr/bin/env python
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Uploads CSV data to brazilian ANAC',
-    'author': 'Leandro Penz',
-    'url': 'https://github.com/lpenz/anac-civ-csv-upload',
-    'author_email': 'lpenz@lpenz.org',
+    'name': 'anac-csv-upload',
     'version': '0.1',
+    'description': 'Uploads CSV data to brazilian ANAC',
+    'author': 'Leandro Lisboa Penz',
+    'author_email': 'lpenz@lpenz.org',
+    'url': 'https://github.com/lpenz/anac-civ-csv-upload',
     'install_requires': ['nose', 'mechanize'],
     'packages': ['anac'],
     'scripts': ['bin/anac-civ-csv-upload'],
-    'name': 'anac-csv-upload'
+    'classifiers': [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'License :: OSI Approved :: '
+          'GNU General Public License v2 or later (GPLv2+)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          ],
+    'license': "GPL2",
 }
 
 setup(**config)
