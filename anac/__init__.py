52b0f11bc8e713bcf94556e05249b2c95e869e02
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-05-17 21:06:47 +0100

    Follow standard license practices

--- anac/__init__.py
+++ anac/__init__.py
@@ -1,4 +1,8 @@
 # encoding: utf-8
+# Copyright (C) 2015 Leandro Lisboa Penz <lpenz@lpenz.org>
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+
 '''Interface com a ANAC'''
 
 import logging
